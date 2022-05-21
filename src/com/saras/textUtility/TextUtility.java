a2054bde8069b44c88d4710b37da1c43ac09bbe7
Saras <78587510+sarasverma@users.noreply.github.com>
2022-05-21 12:32:18 +0530

    Almost done

--- src/com/saras/textUtility/TextUtility.java
+++ src/com/saras/textUtility/TextUtility.java
@@ -10,8 +10,14 @@ import java.awt.datatransfer.StringSelection;
 // Modules for text-to-speech(Freetts)
 import com.sun.speech.freetts.Voice;
 import com.sun.speech.freetts.VoiceManager;
-// Modules for textRecognition
+// Modules for file handling
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+// Modules for textRecognition
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
 // Modules for request API
@@ -39,14 +45,15 @@ class mainFrame extends JFrame implements ActionListener{
 
     // make it global
     private JTextPane textPane;
-    private JButton lowerCaseButton, upperCaseButton, capitalizeButton, sequenceButton;
-    private JButton appendButton, prependButton, removeSpaceButtton, textRecognitionButton, translateButton;
-    private JButton copyButton, clearButton, readButton;
+    private JPanel textContainer, allButtonContainer, buttonUtilityContainer, buttonBasicContainer;
+	private JButton lowerCaseButton, upperCaseButton, capitalizeButton, sequenceButton, replaceButton, 
+    	importButton, exportButton, appendButton, prependButton, removeSpaceButtton, textRecognitionButton, 
+    	translateButton ,themeButton, copyButton, clearButton, readButton;
 
     mainFrame(){
     	
     	// Text area related stuff
-        JPanel textContainer = new JPanel();
+        textContainer = new JPanel();
         textPane = new JTextPane();
         textPane.setText("Put your text here...");
         // ** required this font otherwise other language won\"t work **
@@ -58,10 +65,10 @@ class mainFrame extends JFrame implements ActionListener{
 
 
         // All buttons container
-        JPanel allButtonContainer = new JPanel();
+        allButtonContainer = new JPanel();
 
         // Button Utility container
-        JPanel buttonUtilityContainer = new JPanel();
+        buttonUtilityContainer = new JPanel();
         lowerCaseButton = new JButton("Lower");
         upperCaseButton = new JButton("Upper");
         capitalizeButton = new JButton("Capitalize");
@@ -71,6 +78,9 @@ class mainFrame extends JFrame implements ActionListener{
         removeSpaceButtton = new JButton("Remove Spaces");
         textRecognitionButton = new JButton("Text Recognition");
         translateButton = new JButton("Translate");
+        replaceButton = new JButton("Replace");
+        importButton = new JButton("Import");
+        exportButton = new JButton("Export");
         // adding functionalities to buttonUtilityContainer
         lowerCaseButton.addActionListener(this);
         upperCaseButton.addActionListener(this);
@@ -81,6 +91,9 @@ class mainFrame extends JFrame implements ActionListener{
         removeSpaceButtton.addActionListener(this);
         textRecognitionButton.addActionListener(this);
         translateButton.addActionListener(this); 
+        replaceButton.addActionListener(this); 
+        importButton.addActionListener(this);
+        exportButton.addActionListener(this);
         // adding elements to buttonUtilityContainer
         buttonUtilityContainer.add(lowerCaseButton);
         buttonUtilityContainer.add(upperCaseButton);
@@ -91,44 +104,47 @@ class mainFrame extends JFrame implements ActionListener{
         buttonUtilityContainer.add(removeSpaceButtton);
         buttonUtilityContainer.add(textRecognitionButton);
         buttonUtilityContainer.add(translateButton);
-        buttonUtilityContainer.setLayout(new GridLayout(9, 1, 0, 5));
+        buttonUtilityContainer.add(replaceButton);
+        buttonUtilityContainer.add(importButton);
+        buttonUtilityContainer.add(exportButton);
+        
+        buttonUtilityContainer.setLayout(new GridLayout(12, 1, 0, 5));
         
         // Button basic container
-        JPanel buttonBasicContainer = new JPanel();
+        buttonBasicContainer = new JPanel();
+        themeButton = new JButton("Theme");
         copyButton = new JButton("Copy");
         readButton = new JButton("Read");
         clearButton = new JButton("Clear");
         // adding functionalities to buttonBasicContainer
+        themeButton.addActionListener(this);
         copyButton.addActionListener(this);
         readButton.addActionListener(this);
         clearButton.addActionListener(this);
         // adding elements to buttonBasicContainer
+        buttonBasicContainer.add(themeButton);
         buttonBasicContainer.add(copyButton);
         buttonBasicContainer.add(readButton);
         buttonBasicContainer.add(clearButton);
-        buttonBasicContainer.setLayout(new GridLayout(3, 1, 0, 5));
+        buttonBasicContainer.setLayout(new GridLayout(4, 1, 0, 5));
         
    
         // adding all buttons section to button container
         allButtonContainer.setLayout(new BorderLayout());
         allButtonContainer.add(buttonUtilityContainer, BorderLayout.NORTH);
         allButtonContainer.add(buttonBasicContainer, BorderLayout.SOUTH);
-        allButtonContainer.setBackground(new Color(0.964f, 0.968f, 0.964f, 1f));
 
-
-        // Adding all container to frame
+        
+        // Adding all container to Mainframe
         setLayout(new BorderLayout());
         add(textContainer, BorderLayout.CENTER);
         add(allButtonContainer, BorderLayout.EAST);
 
         
-        
         // frame configuration
         setSize(1200, 600);
         setTitle("Text utility");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //setUndecorated(true);
-        //setBackground(new Color(0.125f, 0.890f, 0.972f, .9f));
         setVisible(true);
         
     }
@@ -139,7 +155,10 @@ class mainFrame extends JFrame implements ActionListener{
         String action = event.getActionCommand();
         
         // basic button implementations
-        if (action.equals("Clear")) {
+        if(action.equals("Theme")) {
+        	setTheme();
+        }
+        else if (action.equals("Clear")) {
             textPane.setText("");
         }
         else if (action.equals("Copy")) {
@@ -227,11 +246,68 @@ class mainFrame extends JFrame implements ActionListener{
         else if(action.equals("Translate")) {
         	translate();
         }
-        // take screenshot of a thing and do stuff
-        // replace and other stuff
-        // import and export file
-        // probably make editor transparent
-        // editor pane ke upar kuch
+        else if (action.equals("Replace")){
+            String text = textPane.getText();
+            // new Panel for asking multiple input in Input box
+            JPanel replacePanel = new JPanel(new GridLayout(4, 3));
+            JLabel findLabel = new JLabel("Find :");
+            JTextField find = new JTextField(10);
+            JLabel replaceLabel = new JLabel("Replace :");
+            JTextField replace = new JTextField(10);
+            replacePanel.add(findLabel);
+            replacePanel.add(find);
+            replacePanel.add(replaceLabel);
+            replacePanel.add(replace);
+            JOptionPane.showMessageDialog(this, replacePanel);
+            String modifiedText = text.replaceAll(find.getText(), replace.getText());
+            textPane.setText(modifiedText);
+        }
+        else if(action.equals("Import")) {
+	    	// choose file
+	    	JFileChooser fileChooser = new JFileChooser();
+	    	fileChooser.setCurrentDirectory(new File("."));  // for current directory
+	    	
+			if(fileChooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+		    	try {
+		    		File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+		    		String newText = "", line = "";
+		    		
+		    		FileReader fileReader = new FileReader(file);
+		    		BufferedReader bufferedReader = new BufferedReader(fileReader);
+		    		
+		    		line = bufferedReader.readLine();
+		    		
+		    		while((line = bufferedReader.readLine()) != null) {
+		    			newText = newText + "\n" + line;
+		    		}
+		    		textPane.setText(textPane.getText() + newText);
+		    	}
+		    	catch(Exception e){
+	        		JOptionPane.showMessageDialog(this,"Not a valid file !");
+	        	}
+			}
+        }        
+        else if(action.equals("Export")) {
+	    	// choose file
+	    	JFileChooser fileChooser = new JFileChooser();
+	    	fileChooser.setCurrentDirectory(new File("."));  // for current directory
+	    	
+			if(fileChooser.showSaveDialog(this) == JFileChooser.APPROVE_OPTION) {
+		    	try {
+		    		File file1 = new File(fileChooser.getSelectedFile().getAbsolutePath());
+		    		
+		    		FileWriter fileWriter = new FileWriter(file1, false);
+		    		BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+		    		
+		    		bufferedWriter.write(textPane.getText()); 
+		    		bufferedWriter.flush();
+		    		bufferedWriter.close(); 
+		    	}
+		    	catch(Exception e){
+	        		JOptionPane.showMessageDialog(this,"Some error occured !");
+	        	}
+			}
+        }
     }
     
     private void speak(){
@@ -272,7 +348,6 @@ class mainFrame extends JFrame implements ActionListener{
 		    	catch(TesseractException e){
 	        		JOptionPane.showMessageDialog(this,"Not a valid file !");
 	        	}
-				
 			}
     	}
     	catch(Exception e) {
@@ -307,40 +382,144 @@ class mainFrame extends JFrame implements ActionListener{
 	    	
         	String lang = (String) JOptionPane.showInputDialog(this, "Choose language to translate in ", "Translate", JOptionPane.QUESTION_MESSAGE,
         			null, langOptions, "English-en");
-            String url = "https://libretranslate.de/translate";
-            String requestJson = String.format("{\"q\": \"%s\", \"source\": \"auto\", \"target\": \"%s\"}", textPane.getText(), lang.split("-")[1]);
-            
-            URL obj = new URL(url);
-            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-            
-            // setting properties
-            con.setConnectTimeout(5000);
-            con.setRequestMethod("POST");
-            con.setRequestProperty("Content-Type", "application/json"); // for setting header
-            con.setDoOutput(true);
-            con.setDoInput(true);
 
-            // sending jsonRequest to server
-            OutputStream os = con.getOutputStream();
-            os.write(requestJson.getBytes("utf-8"));
-            os.close();
-            
-            // for response
-            InputStream in = new BufferedInputStream(con.getInputStream());
-            String response = IOUtils.toString(in, "UTF-8");
-            
-            System.out.println(response);
-            
-            JSONObject responseJson = new JSONObject(response);
-            textPane.setText(responseJson.getString("translatedText"));
-
-            in.close();
-            con.disconnect();
+            if(lang != null){
+            	String url = "https://libretranslate.de/translate";
+            	String requestJson = String.format("{\"q\": \"%s\", \"source\": \"auto\", \"target\": \"%s\"}", textPane.getText(), lang.split("-")[1]);
+            	
+            	URL obj = new URL(url);
+            	HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            	
+            	// setting properties
+            	con.setConnectTimeout(5000);
+            	con.setRequestMethod("POST");
+            	con.setRequestProperty("Content-Type", "application/json"); // for setting header
+            	con.setDoOutput(true);
+            	con.setDoInput(true);
+            	
+            	// sending jsonRequest to server
+            	OutputStream os = con.getOutputStream();
+            	os.write(requestJson.getBytes("utf-8"));
+            	os.close();
+            	
+            	// for response
+            	InputStream in = new BufferedInputStream(con.getInputStream());
+            	String response = IOUtils.toString(in, "UTF-8");
+            	
+            	System.out.println(response);
+            	
+            	JSONObject responseJson = new JSONObject(response);
+            	textPane.setText(responseJson.getString("translatedText"));
+            	
+            	in.close();
+            	con.disconnect();
+            }
         }
             
         catch (Exception e) {
             JOptionPane.showMessageDialog(this, "Error !");
             // System.out.println(e);
         }
+    }
+   
+    private void setTheme() {
+    	// By DEFAULT Light Blue
+    	Color bgContainerColor = new Color(239, 255, 253), bgTextPaneColor = new Color(239, 255, 253),
+    			bgButtonColor = new Color(133, 244, 255);
+    	Color fgTextPaneColor = new Color(44, 51, 51), fgButtonColor = new Color(44, 51, 51);
+    	
+    	String themeOptions[] = {"Light Blue", "Light Pink", "Light Green", "Dark Blue", "Dark Red", "Dark Green"};
+    	String theme = (String) JOptionPane.showInputDialog(this, "Choose Theme ", "Theme Selection", JOptionPane.QUESTION_MESSAGE,
+    			null, themeOptions, "Light");
+    	if(theme == null) {return;}
+    	else if(theme.equals("Light Blue")){	
+    		bgContainerColor = new Color(239, 255, 253);
+    		bgTextPaneColor = new Color(239, 255, 253);
+    		bgButtonColor = new Color(133, 244, 255);
+    		fgTextPaneColor = new Color(44, 51, 51);
+    		fgButtonColor = new Color(44, 51, 51);
+    	}
+    	else if (theme.equals("Light Pink")) {
+    		bgContainerColor = new Color(254, 227, 236);
+    		bgTextPaneColor = new Color(249, 197, 213);
+    		bgButtonColor = new Color(242, 120, 159);
+    		fgTextPaneColor = new Color(44, 51, 51);
+    		fgButtonColor = new Color(44, 51, 51);
+    	}
+    	else if (theme.equals("Light Green")) {
+    		bgContainerColor = new Color(184, 241, 176);
+    		bgTextPaneColor = new Color(227, 252, 191);
+    		bgButtonColor = new Color(20, 195, 142);
+    		fgTextPaneColor = new Color(44, 51, 51);
+    		fgButtonColor = new Color(44, 51, 51);
+    	}
+    	else if (theme.equals("Dark Blue")) {
+    		bgContainerColor = new Color(81, 85, 133);
+    		bgTextPaneColor = new Color(50, 64, 123);
+    		bgButtonColor = new Color(21, 25, 101);
+    		fgTextPaneColor = new Color(238, 238, 238);
+    		fgButtonColor = new Color(238, 238, 238);
+    	}
+    	else if (theme.equals("Dark Red")) {
+    		bgContainerColor = new Color(152, 50, 90);
+    		bgTextPaneColor = new Color(87, 5, 48);
+    		bgButtonColor = new Color(76, 0, 39);
+    		fgTextPaneColor = new Color(238, 238, 238);
+    		fgButtonColor = new Color(238, 238, 238);
+    	}
+    	else if (theme.equals("Dark Green")) {
+    		bgContainerColor = new Color(58, 145, 136);
+    		bgTextPaneColor = new Color(4, 74, 66);
+    		bgButtonColor = new Color(6, 41, 37);
+    		fgTextPaneColor = new Color(238, 238, 238);
+    		fgButtonColor = new Color(238, 238, 238);
+    	}
+    	
+    	// textPane
+    	textPane.setBackground(bgTextPaneColor);
+    	textPane.setForeground(fgTextPaneColor);
+    	
+    	// Containers 
+		allButtonContainer.setBackground(bgContainerColor);
+		buttonUtilityContainer.setBackground(bgContainerColor);
+		buttonBasicContainer.setBackground(bgContainerColor);
+		
+		// Buttons
+		lowerCaseButton.setBackground(bgButtonColor);
+		upperCaseButton.setBackground(bgButtonColor);
+		capitalizeButton.setBackground(bgButtonColor);
+		sequenceButton.setBackground(bgButtonColor);
+		appendButton.setBackground(bgButtonColor);
+		prependButton.setBackground(bgButtonColor);
+		removeSpaceButtton.setBackground(bgButtonColor);
+		textRecognitionButton.setBackground(bgButtonColor);
+		translateButton.setBackground(bgButtonColor);
+		themeButton.setBackground(bgButtonColor);
+		copyButton.setBackground(bgButtonColor);
+		clearButton.setBackground(bgButtonColor);
+		readButton.setBackground(bgButtonColor);
+		replaceButton.setBackground(bgButtonColor);
+		replaceButton.setBackground(bgButtonColor);
+		importButton.setBackground(bgButtonColor);
+		exportButton.setBackground(bgButtonColor);
+		
+		// setting foreground of buttons
+		lowerCaseButton.setForeground(fgButtonColor);
+		upperCaseButton.setForeground(fgButtonColor);
+		capitalizeButton.setForeground(fgButtonColor);
+		sequenceButton.setForeground(fgButtonColor);
+		appendButton.setForeground(fgButtonColor);
+		prependButton.setForeground(fgButtonColor);
+		removeSpaceButtton.setForeground(fgButtonColor);
+		textRecognitionButton.setForeground(fgButtonColor);
+		translateButton.setForeground(fgButtonColor);
+		themeButton.setForeground(fgButtonColor);
+		copyButton.setForeground(fgButtonColor);
+		clearButton.setForeground(fgButtonColor);
+		readButton.setForeground(fgButtonColor);
+		replaceButton.setForeground(fgButtonColor);
+		importButton.setForeground(fgButtonColor);
+		exportButton.setForeground(fgButtonColor);
+
     }
 }
